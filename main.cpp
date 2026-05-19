123e5454b7db9905696604e67e9b4b81a1d8c8ea
Anika Akter Ayna <153622582+DevAnika649@users.noreply.github.com>
2026-05-19 22:38:55 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,59 +1,34 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
 #include <string>
+
 using namespace std;
 
-bool isKeyword(string token) {
-    if(token=="int" || token=="float" || token=="if" || token=="else" || token=="for" || token=="while")
-        return true;
-    return false;
-}
 
-bool isNumber(string token) {
-    for(int i=0;i<token.length();i++){
-        if(!isdigit(token[i]))
+bool isAccepted(const string& str) {
+    for (char ch : str) {
+        if (ch != 'a' && ch != 'b') {
             return false;
+        }
     }
     return true;
 }
 
-bool isIdentifier(string token) {
-    if(isalpha(token[0]))
-        return true;
-    return false;
-}
-
 int main() {
+    string input;
+    cout << "DFA for (a+b)*" << endl;
+    cout << "Enter strings (or 'exit' to quit):" << endl;
 
-    ifstream file("input.txt");
-    string line, token;
+    while (true) {
+        cout << "\nInput string: ";
+        getline(cin, input);
 
-    while(getline(file,line)) {
+        if (input == "exit") break;
 
-        stringstream ss(line);
-
-        while(ss >> token) {
-
-            if(isKeyword(token))
-                cout << token << " -> Keyword" << endl;
-
-            else if(isNumber(token))
-                cout << token << " -> Number" << endl;
-
-            else if(isIdentifier(token))
-                cout << token << " -> Identifier" << endl;
-
-            else
-                cout << token << " -> Symbol" << endl;
+        if (isAccepted(input)) {
+            cout << "Result: ACCEPTED " << endl;
+        } else {
+            cout << "Result: REJECTED (Only 'a' and 'b' allowed)" << endl;
         }
     }
-
-    file.close();
+    return 0;
 }
-
-
-
-
-
-
